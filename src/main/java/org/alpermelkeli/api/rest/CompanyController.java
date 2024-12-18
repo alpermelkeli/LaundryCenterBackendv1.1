f00736230cf23a6932ac2c3a0e3623d61fc677fa
alpermelkeli <108495629+alpermelkeli@users.noreply.github.com>
2024-12-18 20:38:46 +0300

    Api key feature added.

--- src/main/java/org/alpermelkeli/api/rest/CompanyController.java
+++ src/main/java/org/alpermelkeli/api/rest/CompanyController.java
@@ -4,6 +4,7 @@ import org.alpermelkeli.model.Company;
 import org.alpermelkeli.service.CompanyService;
 import org.alpermelkeli.service.MachineTimeController;
 import org.alpermelkeli.service.MqttControllerService;
+import org.alpermelkeli.utils.RequiresApiKey;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,26 +24,28 @@ public class CompanyController {
     @Autowired
     private MachineTimeController machineTimeController;
 
+    @RequiresApiKey
     @GetMapping("/getCompanies")
     public List<Company> getAllCompanies() {
         return companyService.getCompanies();
     }
 
+    @RequiresApiKey
     @GetMapping("/getCompanyPrice")
     public double getCompanyPrice(@RequestParam String companyId) {
         return companyService.getCompanyPrice(companyId);
     }
-
+    @RequiresApiKey
     @GetMapping("/getDevices")
     public List<Company.Device> getDevices(@RequestParam String companyId) {
         return companyService.findDevicesByCompanyId(companyId);
     }
-
+    @RequiresApiKey
     @GetMapping("/getMachine")
     public Company.Device.Machine getMachine(@RequestParam String companyId, @RequestParam String deviceId, @RequestParam String machineId) {
         return companyService.getMachine(companyId, deviceId, machineId);
     }
-
+    @RequiresApiKey
     @PostMapping("/increaseMachineTime")
     public void increaseMachineTime(@RequestParam String companyId, @RequestParam String deviceId, @RequestParam String machineId, @RequestParam long time) {
         companyService.increaseMachineTime(companyId, deviceId, machineId, time, ((currentTimeMillis, durationTime) -> {
@@ -54,7 +57,7 @@ public class CompanyController {
 
     }
 
-
+    @RequiresApiKey
     @PostMapping("/turnOn")
     public String turnOnRelay(@RequestParam String companyId, @RequestParam String deviceId, @RequestParam String relayNo, @RequestParam String time) {
         mqttControllerService.sendTurnOnOffCommand(deviceId, relayNo, "ON");
@@ -67,14 +70,14 @@ public class CompanyController {
         });
         return "Success";
     }
-
+    @RequiresApiKey
     @PostMapping("/turnOff")
     public String turnOffRelay(@RequestParam String companyId, @RequestParam String deviceId, @RequestParam String relayNo) {
         mqttControllerService.sendTurnOnOffCommand(deviceId, relayNo, "OFF");
         companyService.updateMachineStatus(companyId, deviceId, relayNo, false);
         return "Success";
     }
-
+    @RequiresApiKey
     @GetMapping("/getTime")
     public long getTime(){
         return System.currentTimeMillis();
