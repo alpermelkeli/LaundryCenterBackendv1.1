804bad46294563c3c6a4fd138fbda06d06afc8c4
alpermelkeli <108495629+alpermelkeli@users.noreply.github.com>
2024-12-29 18:53:59 +0300

    aws deployment successfully

--- src/main/java/org/alpermelkeli/security/SecurityConstants.java
+++ src/main/java/org/alpermelkeli/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package org.alpermelkeli.security;
 
 public class SecurityConstants {
-    public static final long JWTExpiration = 6*600000; //60 minutes
+    public static final long JWTExpiration = 60*6*10000; //60 minutes
     public static final long JWTRefreshExpiration = 4L *604800000; //28 days
     public static final long rateLimitToken = 4L; //try count
     public static final long rateLimitDuration = 15L; //minute
